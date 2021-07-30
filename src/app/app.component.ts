77f69557691c2ccd741d3d473894ab3d41482485
camilo penagos <penagoscamilo99@gmail.com>
2021-07-30 17:06:45 -0500

    form and insert, ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,15 +7,43 @@ import {Task} from './model/task'
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'Angular-TodoList';
 
-  tareaUno:Task = new Task(1,"tarea","tarea de prueba","alta"); 
-  tareaDos:Task = new Task(2,"tarea dos","tarea de prueba","alta"); 
+  tareaUno:Task = new Task(1,"tarea","tarea de prueba","alta",false); 
+  tareaDos:Task = new Task(2,"tarea dos","tarea de prueba","alta",false);
 
-  private tasks:Task[]= [this.tareaUno, this.tareaDos];
+  currenTask:Task = new Task(0,"","","",false);
+
+  tasks:Task[]= [this.tareaUno, this.tareaDos];
+
+  estado=1;
 
  public getTasks():Task[] {
    return this.tasks;
  }
+
+  //Agregar tarea
+  addTask(){
+    this.currenTask.id=this.tasks.length+1;
+    this.tasks.push(this.currenTask);
+    this.currenTask = new Task(0,"","","",false);
+  }
+
+  //Marcar tarea como realizada
+  doTask(task:Task){
+     task.status=true;
+  }
+
+  //eliminar tarea
+  deleteTask(id:number){
+    alert("Eliminada:" + id);
+  }
+
+  //Editar tarea
+  editTask(task:Task){
+    this.currenTask=task;
+  }
+
  
 }
--- src/app/model/task.ts
+++ src/app/model/task.ts
@@ -1,31 +1,59 @@
 export class Task {
-   private id:number;
-   private title:string;
-   private description:string;
-   private priority:string;
+   private _id:number;
+   private _title:string;
+   private _description:string;
+   private _priority:string;
+   private _status : boolean;
 
-   constructor(id:number,title:string,description:string,priority:string){
-        this.id=id;
-        this.title=title;
-        this.description=description;
-        this.priority=priority;
+   
+   constructor(id:number,title:string,description:string,priority:string,status:boolean){
+        this._id=id;
+        this._title=title;
+        this._description=description;
+        this._priority=priority;
+        this._status=status;
    }
 
    //Getters and Setters
 
-   public getId():number{
-       return this.id
+   public get id():number{
+       return this._id
    }
 
-   public getTitle():string{
-    return this.title
+   public set id(id:number){
+    this._id = id
+  }
+
+   public get title():string{
+    return this._title
    }
 
-   public getDescription():string{
-    return this.description;
+   public set title(title:string){
+     this._title = title
+   }
+
+   public get description():string{
+    return this._description;
     }
 
-    public getPriority():string{
-        return this.priority;
+    public set description(description:string){
+        this._description = description
+      }
+
+    public get priority():string{
+        return this._priority;
     }
+
+    public set priority(priority:string){
+        this._priority = priority;
+    }
+    
+    public get status() : boolean {
+        return this._status;
+    }
+    public set status(v : boolean) {
+        this._status = v;
+    }
+
+   
 }
